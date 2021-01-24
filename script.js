d49121d480c0a53c89d538d1d2d6cb771d11e2bf
matthewstclaire <matthew.crnich@gmail.com>
2021-01-24 02:07:50 -0700

    added logic surrounding the game over

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = number;
 
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
@@ -11,12 +13,25 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent =
       "Wait...You actually won...";
   } else if (guess > number) {
-    document.querySelector(".message").textContent = "Your guess it too high.";
+    if (score > 0) {
+      document.querySelector(".message").textContent =
+        "Your guess it too high.";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+    }
   } else if (guess < number) {
-    document.querySelector(".message").textContent = "Your guess it too low.";
+    if (score === 0) {
+      document.querySelector(".message").textContent = "You lost the game";
+    } else {
+      document.querySelector(".message").textContent = "Your guess it too low.";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
   }
 });
 
 // document.querySelector(".message").textContent = document.querySelector(".guess").value;
 
-document.querySelector("#background-color") = green;
+// document.querySelector("#background-color") = green;
