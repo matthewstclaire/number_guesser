a110b2d0ccac9c333b76619a46038d7f5a5c4dbc
matthewstclaire <matthew.crnich@gmail.com>
2021-01-24 01:34:01 -0700

    basic game logic implemented

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent =
       "You have to actually choose a number...";
-  } else document.querySelector(".message").textContent = document.querySelector(".guess").value;
+  } else if (guess === number) {
+    document.querySelector(".message").textContent =
+      "Wait...You actually won...";
+  } else if (guess > number) {
+    document.querySelector(".message").textContent = "Your guess it too high.";
+  } else if (guess < number) {
+    document.querySelector(".message").textContent = "Your guess it too low.";
+  }
 });
+
+// document.querySelector(".message").textContent = document.querySelector(".guess").value;
+
+document.querySelector("#background-color") = green;
