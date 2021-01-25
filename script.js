03f829350cfda23c14b55ee0844c1272475184fe
matthewstclaire <matthew.crnich@gmail.com>
2021-01-25 00:19:32 -0700

    highscore seems to be functioning

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+let number = Math.trunc(Math.random() * 20) + 1;
+//uncomment to see the number
+//document.querySelector(".number").textContent = number;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+  let guess = Number(document.querySelector(".guess").value);
+
+  document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".message").textContent =
+      "Thanks for playing again. Choose a number.";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?"; //
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".guess").value = "";
+  });
 
   if (!guess) {
     document.querySelector(".message").textContent =
       "You have to actually choose a number...";
+    document.querySelector(".guess").value = "";
     //winner
   } else if (guess === number) {
     document.querySelector(
@@ -16,18 +31,25 @@ document.querySelector(".check").addEventListener("click", function () {
     ).textContent = `You win! Your score is ${score}!`;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = number;
+    if (score > highscore) {
+      document.querySelector(".highscore").textContent = score;
+    }
     //devil secret beginning
   } else if (guess === 666) {
     document.querySelector(".message").textContent = "Okay Damien try again!";
     document.querySelector("body").style.backgroundColor = "#b30f04";
+    document.querySelector(".guess").value = "";
   } else if (guess > number) {
     if (score > 0) {
       document.querySelector(".message").textContent =
         "Your guess it too high.";
       score--;
       document.querySelector(".score").textContent = score;
+      document.querySelector(".guess").value = "";
     } else {
       document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".guess").value = "";
     }
   } else if (guess < number) {
     if (score === 0) {
@@ -35,6 +57,7 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       document.querySelector(".message").textContent = "Your guess it too low.";
       score--;
+      document.querySelector(".guess").value = "";
       document.querySelector(".score").textContent = score;
     }
   }
